99f3ac719fa80c90b83724e232e08ef1ff58f994
tothricsaj <tothricsaj@gmail.com>
2019-09-02 14:13:12 +0200

    module/pycache into gitignore

---
+++ modules/bars.py
@@ -0,0 +1,26 @@
+"""
+Bars Module
+===========
+This is an example module due to learning!!!!
+"""
+
+def starbar(num):
+    """Prints a bar with *
+
+    :arg num: Length of the bar
+    """
+    print('*' * num)
+
+def hashbar(num):
+    """Prints a bar with #
+
+    :arg num: Length of the bar
+    """
+    print('#' * num)
+
+def simplebar(num):
+    """Prints a bar with -
+
+    :arg num: Length of the bar
+    """
+    print('-' * num)
