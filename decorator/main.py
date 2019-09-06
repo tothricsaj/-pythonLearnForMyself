38593149367c26711dbb659d9bcad60202cc788a
tothricsaj <tothricsaj@gmail.com>
2019-09-06 16:24:41 +0200

    decorator with arguments

--- decorator/main.py
+++ decorator/main.py
@@ -14,8 +14,24 @@ def add(a, b):
     'Add two numbers'
     return a + b
 
+def star_decorator(count_of_stars):
+    def wrapped_func(func):
+        def wrapper(*args, **kwargs):
+            print('*' * count_of_stars)
+            func(*args, **kwargs)
+            print('*' * count_of_stars)
+        return wrapper
+    return wrapped_func
+
+
+@star_decorator(40)
+def plot_txt(txt):
+    print(txt)
+
 
 if __name__ == '__main__':
     print('hello decorator!!!\n')
     
     print(add(1,3))
+
+    plot_txt('Hello Decorator!!!!')
