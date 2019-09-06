e1403f3d625449cd8d401ae870445a9a053b6aaa
tothricsaj <tothricsaj@gmail.com>
2019-09-06 12:35:27 +0200

    alphabet iterator

--- iterator/main.py
+++ iterator/main.py
@@ -16,5 +16,35 @@ class Counter(object):
       return self.current - 1
         
 
+class GetAlphabet(object):
+  def __init__(self, beginChar):
+      """
+      It will rais AttributeError if the attribute is not str and longer than 1'
+      """
+      if isinstance(beginChar, str) and len(beginChar) == 1:
+          self.beginChar = beginChar
+      else:
+          raise AttributeError
+
+  def __iter__(self):
+    'Returns itself as an iterator object'
+    print('Iterator is birthing.....\n')
+    return self
+
+  def __next__(self):
+    'Returns the next value till current is lower than high'
+    if ord(self.beginChar) >= 123:
+      raise StopIteration
+    else:
+      self.beginChar = chr(ord(self.beginChar) + 1)
+      return chr(ord(self.beginChar) - 1)
+
+
+
 if __name__ == '__main__':
-    print('Hello iterator')
+    print('Hello iterator\n')
+
+    ga = GetAlphabet('g')
+
+    for i in ga:
+        print(i, end=" - ")
