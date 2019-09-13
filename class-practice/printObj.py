a37413e25ed41775609b8708f5c4ab26c6c86400
tothricsaj <tothricsaj@gmail.com>
2019-09-13 16:09:52 +0200

    __str__ and __repr__

--- class-practice/printObj.py
+++ class-practice/printObj.py
@@ -1,6 +1,21 @@
 class Test:
-    pass
+
+    __my_private = 'I am a private variable!'
+    
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def __repr__(self):
+        return 'This is a prnt object example. The a -> {} and the b -> {}'.format(self.a, self.b)
+
+    def __str__(self):
+        return 'This is a prnt object example. The a -> {} and the b -> {}'.format(self.a, self.b)
+
 
 
 if __name__ == '__main__':
-    print('Hello belloooo!!!')
+    print('Hello belloooo!!!\n')
+    t = Test(2,3)
+    print(t)
+    print([t])
