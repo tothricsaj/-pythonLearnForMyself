f94b1fc5412bdadc228af9fa67bbb976e25c4364
tothricsaj <tothricsaj@gmail.com>
2019-09-15 23:45:59 +0200

    polymorphism

--- variableNaming/main.py
+++ variableNaming/main.py
@@ -11,22 +11,17 @@ class T():
     def total_private(self, new_str):
         self.__totalPrivat = new_str
 
-    def update(self):
+    def __update(self):
         print('This is update function from T class!')
-
-    __update = update
-
 
 
 class T_Sub(T):
     def __init__(self):
         T.__init__(self)
 
-    def update(self, msg):
+    def __update(self, msg):
         print(msg)
 
-    def invoke_T_update(self):
-        T.update(self)
 
 
 if __name__ == '__main__':
@@ -43,4 +38,3 @@ if __name__ == '__main__':
     print(t.total_private)
 
     t_sub.update('I am an individual function of T_Sub class!\n')
-    t_sub.invoke_T_update()
