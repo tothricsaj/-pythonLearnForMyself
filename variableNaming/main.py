874dde581387d1685f5cc7f0e1ab7b2095604adb
tothricsaj <tothricsaj@gmail.com>
2019-09-15 23:29:55 +0200

    update function is added - update nothig owing to only inheritence exemple

--- variableNaming/main.py
+++ variableNaming/main.py
@@ -3,10 +3,36 @@ class T():
         self._myPrivat = 'I am private...'
         self.__totalPrivat = 'I am real private....'
 
+    @property
+    def total_private(self):
+        return self.__totalPrivat
+
+    @total_private.setter
+    def total_private(self, new_str):
+        self.__totalPrivat = new_str
+
+    def update(self):
+        print('This is update function from T class!')
+
+
+
+class T_Sub(T):
+    def __init__(self):
+        T.__init__(self)
+
 
 if __name__ == '__main__':
     print('hello naming\n')
 
     t = T()
-    print(t._myPrivat)
-    print(t.__totalPrivat)
+    t_sub = T_Sub()
+
+    print(t_sub._myPrivat)
+    print(t.total_private)
+
+    t.total_private = 'I am swaped....sad    :('
+
+    print(t.total_private)
+
+    t_sub.update()
+
