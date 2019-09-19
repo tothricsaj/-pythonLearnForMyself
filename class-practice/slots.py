5493068473fcec3cda5f805096f7ddc9c5bc2249
tothricsaj <tothricsaj@gmail.com>
2019-09-19 13:05:28 +0200

    slot practice

--- class-practice/slots.py
+++ class-practice/slots.py
@@ -1,27 +1,17 @@
 class SlotClass(object):
-    __slots__ = ('a')
 
+    __slots__ = ['val']
 
-class SlotWithDict():
-    __slots__ = ('__dict__', 'b')
+    def __init__(self, val):
+        self.val = val
 
 
 if __name__ == '__main__':
     print('Hello slots!!!!')
 
-    sc = SlotClass()
-    sc.a = 22
+    sc = SlotClass(42)
 
-    print(sc.a)
+    print(sc.val)
 
     print('\n')
 
-    swd = SlotWithDict()
-    swd.a = 23
-    swd.b = 'Susi musi'
-    swd.c = 42
-
-    print(swd.a)
-    print(swd.b)
-    print(swd.c)
-    print(swd.__dict__)
