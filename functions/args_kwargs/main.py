ff447a40356352d144e5db5bb0762cdc734a2397
tothricsaj <tothricsaj@gmail.com>
2019-09-23 14:04:02 +0200

    kwargs

--- functions/args_kwargs/main.py
+++ functions/args_kwargs/main.py
@@ -4,7 +4,16 @@ def func1(*args):
     else:
         print('End of the methode.... it is so sad.... :(')
 
+def func2(**kwargs):
+    for key, value in kwargs.items():
+        print('{} ----> {}'.format(key, value))
+
+    else:
+        print('it is done!!!!')
+
 if __name__ == '__main__':
     print('Hello multiple arguments!!!!\n')
 
     func1('dogy', 'kitty', 'mesaure failure')
+
+    func2(foo='bar', food='bars')
