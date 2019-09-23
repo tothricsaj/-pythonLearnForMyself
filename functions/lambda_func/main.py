b486abdc4be664745e631d3ee6d76d0c35532f58
tothricsaj <tothricsaj@gmail.com>
2019-09-23 13:27:36 +0200

    bread rows

--- functions/lambda_func/main.py
+++ functions/lambda_func/main.py
@@ -1,5 +1,16 @@
 double = lambda x : 2*x
 
+my_list = [1, 2, 5, 3, 22, 11, 30, 48]
+
 if __name__ == '__main__':
     print('Hello lambda!!!!!!!!\n')
     print(double(3))
+
+    new_list = list(
+            filter(
+                lambda x: (x%2==0), 
+                my_list
+            )
+        )
+
+    print(new_list)
