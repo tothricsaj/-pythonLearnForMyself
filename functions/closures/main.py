4c3b50ac1998ef642427abb2affdda3545d6d599
tothricsaj <tothricsaj@gmail.com>
2019-09-24 12:10:15 +0200

    logger function

--- functions/closures/main.py
+++ functions/closures/main.py
@@ -1,11 +1,31 @@
-def outerFunc(txt):
-    def innerFunc(iTxt):
-        print(txt + " " + iTxt)
+import logging
+logging.basicConfig(filename='example.log', level=logging.INFO)
 
-    return innerFunc
+def logger(func):
+    def log_func(*args):
+        logging.info(
+                'Running "{}" with arguments {}'.format(func.__name__, args)
+                )
+
+        print(func(*args))
+
+    return log_func
+
+def add(x, y):
+    return x + y
+
+def sub(x, y):
+    return x - y
 
 if __name__ == '__main__':
     print('hello closure!!!\n')
 
-    my_func = outerFunc('hey!!!')
-    my_func('honyDucky')
+    add_logger = logger(add)
+    sub_logger = logger(sub)
+
+    add_logger(2, 3)
+    add_logger(11, 124)
+
+    sub_logger(3,2)
+    sub_logger(2323, 121)
+
