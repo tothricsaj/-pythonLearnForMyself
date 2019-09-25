1e58fca5271e78ee41f0046f64ab577c545707c4
tothricsaj <tothricsaj@gmail.com>
2019-09-25 11:18:04 +0200

    Helper class to test help function

--- functions/help_function/main.py
+++ functions/help_function/main.py
@@ -1,4 +1,20 @@
+class Helper():
+    def __init__(self):
+        """
+        The helper class init :D
+        """
+
+    def print_help(self):
+        """
+        This is the print help function
+        owing to fun.....
+        """
+
+        print('I am helping to print help!')
+
 if __name__ == '__main__':
     print('Hello helper!!!\n')
 
-    help()
+    help(Helper)
+    help(Helper.print_help)
+
